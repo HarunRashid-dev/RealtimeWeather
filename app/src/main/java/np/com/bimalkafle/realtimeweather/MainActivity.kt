6628248d9571f5865f5f5e64125d161b97e33cbf
Admin <harunrashid9159@gmail.com>
2024-12-30 08:57:00 +0530

    Code still in process and completed till 8:07, Project taken from Yourtube Video Link :https://www.youtube.com/watch?v=Kn6vUH1uJT4&list=PLgpnJydBcnPA5aNrlDxxKWSqAma7m3OIl&index=9

---
+++ app/src/main/java/np/com/bimalkafle/realtimeweather/MainActivity.kt
@@ -0,0 +1,24 @@
+package np.com.bimalkafle.realtimeweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import np.com.bimalkafle.realtimeweather.ui.theme.RealtimeWeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherPage()
+        }
+    }
+}
